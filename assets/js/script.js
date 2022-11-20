1be48ff141f2114c220e95debc58376dd92a4abf
narizon35 <recinoswilfredo@gmail.com>
2022-11-20 00:06:27 -0800

    modified html added Javascript added variables

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+var wordBlank = document.querySelector(".word-blanks");
+var win = document.querySelector(".win");
+var lose = document.querySelector(".lose");
+var timerElement = document.querySelector(".timer-count");
+var startButton = document.querySelector(".start-button");
